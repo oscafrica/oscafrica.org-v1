a734431b4f9a8e39baa54c5007fc43f9b08d26b7
Samson Goddy <samsongoddy@sugarlabs.org>
2018-07-17 18:25:28 +0100

    Added time

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,9 +18,15 @@
 		xxsmall: '(max-width: 360px)'
 	});
 
-	$(window).on("load",function(){
-		$('#OSCA').fadeOut('slow',function(){$(this).remove();});
-	});
+	document.onreadystatechange = function () {
+		var state = document.readyState
+		if (state == 'complete') {
+			setTimeout(function(){
+				document.getElementById('interactive');
+			   document.getElementById('OSCA').style.visibility="hidden";
+			},9000);
+		}
+	  }
 
 
 	
